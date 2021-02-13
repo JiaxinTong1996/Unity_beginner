7eb53ddc7536129dd9e5c28058709de92e1f670e
Jiaxin Tong <tong.jiax@northeastern.edu>
2021-02-12 21:43:37 -0500

    update

--- Roll_a_ball/Assets/_Script/CameraController.cs
+++ Roll_a_ball/Assets/_Script/CameraController.cs
@@ -7,16 +7,20 @@ public class CameraController : MonoBehaviour
 {
     Vector3 offset;
     public Transform player;
+    AudioSource background;//
     // Start is called before the first frame update
     void Start()
     {
         offset = player.position - transform.position;
+        background = GetComponent<AudioSource>();
+        background.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = player.position - offset;
+
     }
     public void Restart()
     {
