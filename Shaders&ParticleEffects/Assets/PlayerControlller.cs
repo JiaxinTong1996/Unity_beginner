20a9e28d3e8f4b8f07a0de42a6b42a08af63c372
Jiaxin Tong <tong.jiax@northeastern.edu>
2021-03-31 01:28:03 -0400

    up

---
+++ Shaders&ParticleEffects/Assets/PlayerControlller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlller : MonoBehaviour
+{
+
+    float h;
+    float v;
+    Rigidbody rb;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Vector3 force = new Vector3(h,0,v);
+        force *= speed;
+        rb.AddForce(force);
+    }
+}
